89ce90540935f839465e9b23c220d05201d30e66
Ivan Martinez-Kay <martinezkayivan@gmail.com>
2024-06-17 22:10:18 -0400

    navbar

--- AppArch/frontend/BMFT/src/components/Nav.js
+++ AppArch/frontend/BMFT/src/components/Nav.js
@@ -1,21 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { TfiAngleDown } from "react-icons/tfi";
+
 
 const Nav = () => {
+
   return (
     <div>
       <header className="bg-green-500"> 
-        <nav>
+        <nav className='flex justify-between items-center w-[92%] mx-auto'>
           <div>
 
           </div>
-          <div>
-            <ul class ="flex items-center gap-[4vw]">
-            <li><a href = "/">Home</a></li>
-            <li><a href = "/FAQ">What We Do</a></li>
-            <li><a href = "/News">News</a></li>
-            <li><a href = "/Involvement">Get Involved</a></li>
-            </ul>
-          </div>
+          
+          <div className='md:static absolute bg-green-500 md:min-h-fit min-h-[60vh left-0 top-0 md:w-auto w-full flex items-center px-5'>
+            <ul className ="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-6">
+            <li><a className = "hover:text-gray-500" href = "/">Home</a></li>
+            <li><a className = "hover:text-gray-500" href = "/FAQ">What We Do</a></li>
+            <li><a className = "hover:text-gray-500" href = "/News">News</a></li>
+            <li><a className = "hover:text-gray-500" href = "/Involvement">Get Involved</a></li>
+            </ul>  
+          </div> 
+
         </nav>
       </header>
     </div>
