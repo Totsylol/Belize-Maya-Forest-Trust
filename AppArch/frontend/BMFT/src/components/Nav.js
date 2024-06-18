2d529c0118d3eaa75ebec4b23f62df6f989c1bf9
Ivan Martinez-Kay <martinezkayivan@gmail.com>
2024-06-17 20:50:10 -0400

    navbar

--- AppArch/frontend/BMFT/src/components/Nav.js
+++ AppArch/frontend/BMFT/src/components/Nav.js
@@ -3,7 +3,21 @@ import React from 'react'
 const Nav = () => {
   return (
     <div>
-      
+      <header className="bg-green-500"> 
+        <nav>
+          <div>
+
+          </div>
+          <div>
+            <ul class ="flex items-center gap-[4vw]">
+            <li><a href = "/">Home</a></li>
+            <li><a href = "/FAQ">What We Do</a></li>
+            <li><a href = "/News">News</a></li>
+            <li><a href = "/Involvement">Get Involved</a></li>
+            </ul>
+          </div>
+        </nav>
+      </header>
     </div>
   )
 }
