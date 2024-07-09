63d8967eff16c572451319cd1920309a6b089376
Ivan Martinez-Kay <martinezkayivan@gmail.com>
2024-07-08 20:45:40 -0400

    navbar

--- AppArch/frontend/BMFT/src/components/Home.js
+++ AppArch/frontend/BMFT/src/components/Home.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import styles from "./styles/home.module.css"
+import home from "./assets/Home.jpg"
 
 const Home = () => {
   return (
-    <div className="text-3xl font-bold underline">
+    <div>
+      <img src={home} className={styles.img}></img>
       <h1>Hello</h1>
     </div>
   )
