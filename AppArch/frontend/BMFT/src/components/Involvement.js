7272d7c174f92ae234aa6145055e4bb23db60975
Totsylol <thefishjonah@gmail.com>
2024-07-16 15:00:06 -0400

    sub button added

--- AppArch/frontend/BMFT/src/components/Involvement.js
+++ AppArch/frontend/BMFT/src/components/Involvement.js
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import Footer from './Footer'
 const Involvement = () => {
   return (
+    
     <div>
       <h1>Involvement</h1>
+      <Footer/>
     </div>
   )
+  
 }
 
 export default Involvement
