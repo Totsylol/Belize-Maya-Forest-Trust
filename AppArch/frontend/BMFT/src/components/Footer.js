f30c89152d71cfd6c030f1d231cd0e070b8881e4
Totsylol <thefishjonah@gmail.com>
2024-07-31 19:31:50 -0400

    delete sum stuff

--- AppArch/frontend/BMFT/src/components/Footer.js
+++ AppArch/frontend/BMFT/src/components/Footer.js
@@ -64,48 +64,8 @@ function Footer() {
             </Link>
           </div>
           <small className={styles.websiterights}> ©Belize Maya Forest Trust 2020</small>
-          <div className={styles.socialicons}>
-            <Link
-              class='social-icon-link facebook'
-              to='/'
-              target='_blank'
-              aria-label='Facebook'
-            >
-              <i class='fab fa-facebook-f' />
-            </Link>
-            <Link
-              class='social-icon-link instagram'
-              to='/'
-              target='_blank'
-              aria-label='Instagram'
-            >
-              <i class='fab fa-instagram' />
-            </Link>
-            <Link
-              class='social-icon-link youtube'
-              to='/'
-              target='_blank'
-              aria-label='Youtube'
-            >
-              <i class='fab fa-youtube' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='Twitter'
-            >
-              <i class='fab fa-twitter' />
-            </Link>
-            <Link
-              class='social-icon-link twitter'
-              to='/'
-              target='_blank'
-              aria-label='LinkedIn'
-            >
-              <i class='fab fa-linkedin' />
-            </Link>
-          </div>
+          
+          
         </div>
       </section>
     </div>
