d17125eaa3e2aea0d6e5e1e850907d1a2975ff59
Totsylol <thefishjonah@gmail.com>
2024-08-25 17:24:00 -0400

    contact us

--- AppArch/frontend/BMFT/src/components/MainPages/FAQ.js
+++ AppArch/frontend/BMFT/src/components/MainPages/FAQ.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import ContactUs from "../ContactUs.js"
 
 const FAQ = () => {
   return (
     <div>
-      <h1>faq</h1>
+         <h1>faq</h1>
+         
+      <ContactUs></ContactUs>
     </div>
   )
 }
