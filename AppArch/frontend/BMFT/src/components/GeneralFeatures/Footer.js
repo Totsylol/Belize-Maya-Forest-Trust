5fe48b247de47605593ac1b754f7a2d6e4332bdf
Totsylol <thefishjonah@gmail.com>
2024-09-01 09:51:24 -0400

    footer

--- AppArch/frontend/BMFT/src/components/GeneralFeatures/Footer.js
+++ AppArch/frontend/BMFT/src/components/GeneralFeatures/Footer.js
@@ -1,72 +1,64 @@
 import React from 'react';
 import styles from '../styles/foot.module.css';
-import { Link } from 'react-router-dom';
 import FinalBMFT from '../assets/FinalBMFT.jpg';
-
 
 function Footer() {
   return (
-    
     <div className={styles.footercontainer}>
       <section className={styles.footersubscription}>
         <p className={styles.footersubscriptiontext}>
         </p>
       </section>
-      <div class='footerlinks'>
+      <div className='footerlinks'>
         <div className={styles.footerlinkwrapper}>
-        <img className={styles.FinalBMFT} src={FinalBMFT}/>
+          <img className={styles.FinalBMFT} src={FinalBMFT} alt="FinalBMFT" />
           <div className={styles.footerlinkitems}>
             <h2>About Us</h2>
-            <Link to='/sign-up'>Our Story</Link>
-            <Link to='/'>Biodiversity</Link>
-            <Link to='/'>Cultural Heritage</Link>
-            <Link to='/'>Conerservation Targets</Link>
-            <Link to='/'>Threats</Link>
+            <a href='/sign-up'>Our Story</a>
+            <a href='/protbio'>Biodiversity</a>
+            <a href='/outreach'>Outreach</a>
+            <a href='/agroproj'>Agriculture Program</a>
+            <a href='/communitystewards'>Community Stewards</a>
           </div>
-          <div class={styles.footerlinkitems}>
+          <div className={styles.footerlinkitems}>
             <h2>Contact Us</h2>
-            <Link to='/'>Contact</Link>
-            <Link to='/'>Support</Link>
-            <Link to='/'>Donate</Link>
-            <Link to='/'></Link>
+            <a href='/'>Contact</a>
+            <a href='/'>Support</a>
+            <a href='/'>Donate</a>
           </div>
-          <div class={styles.footerlinkitems}>
+          <div className={styles.footerlinkitems}>
             <h2>Social Media</h2>
-            <Link to='/'>Email Us</Link>
-            <Link to='/'>Facebook</Link>
-            <Link to='/'>Youtube</Link>
-            
-            
+            <a href='mailto:info@bmft.org.bz'>Email Us</a>
+            <a href='https://www.facebook.com/BelizeMayaForestTrust'>Facebook</a>
+            <a href='https://www.youtube.com/channel/UCWKdyK1WxDc7nKskT_vsEeQ'>YouTube</a>
           </div>
           
           <div className={styles.inputareas}>
             <p className={styles.footersubscriptionheading}>
-        Subscribe to Our Newsletter
-        </p>
-          <form>
-            <input
-              className={styles.footerinput}
-              name='email'
-              type='email'
-              placeholder='Email'
-            />
+              Subscribe to Our Newsletter
+            </p>
+            <form>
+              <input
+                className={styles.footerinput}
+                name='email'
+                type='email'
+                placeholder='Email'
+              />
             </form>
             <button className={styles.subbutton}>
-            Subscribe
-          </button>
-        </div>
+              Subscribe
+            </button>
+          </div>
         </div>
       </div>
       <section className={styles.socialmedia}>
         <div className={styles.socialmediawrap}>
-          <div class='footerlogo'>
-            <Link to='/' className={styles.sociallogo}>
-              <i class='fab fa-typo3' />
-            </Link>
+          <div className='footerlogo'>
+            <a href='/' className={styles.sociallogo}>
+              <i className='fab fa-typo3' />
+            </a>
           </div>
           <small className={styles.websiterights}> ©Belize Maya Forest Trust 2020</small>
-          
-          
         </div>
       </section>
     </div>
