3514b2cb3785572e625f261e827227fe7f258f36
Ivan Martinez-Kay <martinezkayivan@gmail.com>
2024-09-03 11:10:52 -0400

    fixed admin page not loading priperly

--- AppArch/frontend/BMFT/src/App.js
+++ AppArch/frontend/BMFT/src/App.js
@@ -15,7 +15,7 @@ import Outreach from "./components/WWDFeatures/Outreach";
 import Rangers from "./components/BIOPages/Rangers";
 import Staff from "./components/GeneralFeatures/Staff";
 import Communitystewards from './components/WWDFeatures/Communitystewards';
-import AdminNews from "./components/MainPages/AdminNews";
+import AdminNews from "./components/API/AdminNews";
 
 function App() {
   const [isNavbarVisible, setNavbarVisible] = useState(true);
