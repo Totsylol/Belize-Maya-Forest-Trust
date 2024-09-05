c12a76ac69344c404718d784023dca6bfaf552dc
Ivan Martinez-Kay <martinezkayivan@gmail.com>
2024-09-05 12:34:23 -0400

    small change

--- AppArch/frontend/BMFT/src/components/API/Setting.js
+++ AppArch/frontend/BMFT/src/components/API/Setting.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import UpdatePassword from './UpdatePassword'
 
 const Setting = () => {
   return (
     <div>
-      
+        <DeleteUser></DeleteUser>
+        <UpdatePassword></UpdatePassword>
     </div>
   )
 }
