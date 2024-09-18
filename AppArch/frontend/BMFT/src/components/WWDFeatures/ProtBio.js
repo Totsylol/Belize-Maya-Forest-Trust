67abc20764a6f84cd3765b7537d020652ebc6f2a
Totsylol <thefishjonah@gmail.com>
2024-09-18 15:41:44 -0400

    protbio

--- AppArch/frontend/BMFT/src/components/WWDFeatures/ProtBio.js
+++ AppArch/frontend/BMFT/src/components/WWDFeatures/ProtBio.js
@@ -1,15 +1,8 @@
 import React, { useState } from 'react';
 import styles from '../styles/ProtBio.module.css';
-import PBfireteam from '../assets/PBfireteam.jpg';
-import PBCheck from "../assets/PBCheck.jpg";
-import PBfire from "../assets/PBfire.jpg";
-import Placeholder from "../assets/Placeholder.jpg";
 import PBtop from "../assets/PBtop.JPG";
-import PBbird from  "../assets/PBbird.JPG";
-import PBgator from "../assets/PBgator.jpg";
-import OTteam2 from "../assets/OTteam2.JPG";
-import OTplan from "../assets/OTplan.JPG";
-import OTplan2 from "../assets/OTplan2.JPG";
+import WWDPanels3 from './WWDPanels3';
+
 
 const Modal = ({ src, alt, onClose }) => {
     return (
@@ -64,60 +57,11 @@ const ProtBio = () => {
                     </p>
                 </div>
             </section>
-            <section className={styles.Head2}>
-                <header className={styles.header2}>
-                    <img src={PBgator} alt="Fire Prevention" />
-                    <h1 className={styles.titleoverlay2}>Yalbac Water Quality</h1>
-                </header>
-            </section>
-            <section className={styles.descriptionSection}>
-                <h2 className={styles.sectionTitle}>Project Goals</h2>
-                <p className={styles.descriptionText}>
-                The Black Creek, which serves as a tributary to the Belize River and traverses numerous agricultural lands, is currently facing significant challenges due to the adverse effects of chemical and soil runoff. This situation has not only jeopardized the integrity of the freshwater ecosystem but has also adversely affected the local residents of Yalbac community. In partnership with the University of Leeds, Dr. Rachael Carrie organized a series of community workshops aimed at identifying the impacts, challenges, and potential solutions for enhancing the health of the creek. Following these workshops, she undertook the collection of water samples from both the creek and adjacent freshwater sources to perform water quality assessments, thereby gaining insights into effective strategies for improving the creek's condition. The initiative underscored the critical importance of maintaining clean water for the community and sought to address the health and livelihood issues stemming from the extensive application of pesticides and other agricultural chemicals. 
-                </p>
-                <p className={styles.des}>
-                This project is ongoing.  
-                </p>
-            </section>
-            <section className={styles.imageGallery}>
-                <div className={styles.images}>
-                    <img src={OTplan} alt="Planning Water Qaulity" className={styles.image} />
-                    <img src={OTteam2} alt="The Team" className={styles.image} />
-                    <img src={OTplan2} alt="Planning" className={styles.image} />
-                </div>
-            </section>
-            <section className={styles.Head2}>
-                <header className={styles.header2}>
-                    <img src={PBbird} alt="Fire Prevention" />
-                    <h1 className={styles.titleoverlay2}>Fire Prevention</h1>
-                </header>
-            </section>
-            <section className={styles.descriptionSection}>
-                <h2 className={styles.sectionTitle}>Project Goals</h2>
-                <p className={styles.descriptionText}>
-                    Effective fire prevention is crucial for protecting the diverse ecosystems within the Belize Maya Forest. By implementing proactive measures and strategies, such as controlled burns and firebreaks, we can reduce the risk of devastating wildfires. Education and community involvement also play a vital role in ensuring that the local population understands and supports fire prevention efforts. Continued research and innovation in fire management techniques are essential for adapting to changing environmental conditions and safeguarding our natural resources.
-                </p>
-            </section>
-            <section className={styles.imageGallery}>
-                <div className={styles.images}>
-                    <img src={PBfireteam} alt="Habitat Restoration" className={styles.image} />
-                    <img src={PBCheck} alt="Endangered Species" className={styles.image} />
-                    <img src={PBfire} alt="Community Education" className={styles.image} />
-                </div>
-            </section>
-            
-            <section className={styles.videoSection}>
-                <h2>LET FIRE SAFETY START WITH YOU!</h2>
-                <div className={styles.videoContainer}>
-                    <iframe
-                        src="https://www.youtube.com/embed/8kR5UoYsj0o?controls=0&rel=0&modestbranding=1"
-                        title="Learn More About Biodiversity Conservation"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                        className={styles.video}
-                    ></iframe>
-                </div>
-            </section>
+            <section className={`${styles.section2} ${styles.animateRollIn}`}>
+        <header className={styles.banner3}>Join the Journey</header>
+        <WWDPanels3 />
+      </section>
+           
             {modalImage && (
                 <Modal
                     src={modalImage.src}
